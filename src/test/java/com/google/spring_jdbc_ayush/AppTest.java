a82ffd91c715356c04702b66c6b87a29d3c13dbd
Ayush Kumar <ayush.kumar80g@gmail.com>
2023-11-20 16:31:05 +0530

    Maven project setup for spring-jdbc CRUD application

---
+++ src/test/java/com/google/spring_jdbc_ayush/AppTest.java
@@ -0,0 +1,20 @@
+package com.google.spring_jdbc_ayush;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
